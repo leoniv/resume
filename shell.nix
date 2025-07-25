481df33ee5f0a14d3a80294615012089ada1a0d9
Leonid Vlasov <leoniv.vlasov@gmail.com>
2025-07-25 22:40:22 +0700

    fix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,18 @@
 { pkgs ? import <nixpkgs> {} }:
 
-with pkgs;
-
-mkShell {
+pkgs.mkShell {
   buildInputs = [
-    nodejs
+    pkgs.python311
+    pkgs.python311Packages.virtualenv
   ];
+
+  shellHook = ''
+    if [ ! -d .venv ]; then
+      python -m venv .venv
+      .venv/bin/pip install --upgrade pip
+      .venv/bin/pip install "rendercv[full]"
+    fi
+    source .venv/bin/activate
+    echo "✅ Virtualenv activated with rendercv[full]"
+  '';
 }
